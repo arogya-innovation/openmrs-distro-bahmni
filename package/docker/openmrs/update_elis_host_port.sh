4dc2c8135a9d0f97e10ccace60e93c54031e3753
Anish Kumar Yadav <61132432+yadavAnish@users.noreply.github.com>
2025-08-02 18:27:22 +0545

    Update update_elis_host_port.sh

--- package/docker/openmrs/update_elis_host_port.sh
+++ package/docker/openmrs/update_elis_host_port.sh
@@ -2,14 +2,13 @@
 set +e
 
 run_sql() {
-  mysql -N -s --host="${OMRS_DB_HOSTNAME}" --port="${OMRS_DB_PORT}" --user="${OMRS_DB_USERNAME}" --password="${OMRS_DB_PASSWORD}" "${OMRS_DB_NAME}" -e "$1"
+  mysql -N -s --host="${OMRS_DB_HOSTNAME}" --user="${OMRS_DB_USERNAME}" --password="${OMRS_DB_PASSWORD}" "${OMRS_DB_NAME}" -e "$1"
 }
 
-count=$(run_sql "select count(*) from information_schema.tables where table_schema='${OMRS_DB_NAME}' and table_name='markers';")
-
-if [ -n "$count" ] && [ "$count" -gt 0 ]; then
-  echo "Updating OpenELIS Host Port in markers and failed_events table"
-  run_sql "update markers set feed_uri_for_last_read_entry = concat('http://${OPENELIS_HOST}:${OPENELIS_PORT}/openelis/ws/feed/patient',substring(feed_uri_for_last_read_entry, (LENGTH(feed_uri_for_last_read_entry) - LOCATE('/', REVERSE(feed_uri_for_last_read_entry)))+1) ) where feed_uri like '%openelis/ws/feed/patient/recent';"
-  run_sql "update markers set feed_uri = 'http://${OPENELIS_HOST}:${OPENELIS_PORT}/openelis/ws/feed/patient/recent' where feed_uri like '%openelis/ws/feed/patient/recent';"
-  run_sql "update failed_events set feed_uri = 'http://${OPENELIS_HOST}:${OPENELIS_PORT}/openelis/ws/feed/patient/recent' where feed_uri like '%openelis/ws/feed/patient/recent';"
+if [ $(run_sql "select count(*) from information_schema.tables where table_schema='${OMRS_DB_NAME}' and table_name='markers';") -gt 0 ]
+then
+    echo "Updating OpenELIS Host Port in markers and failed_events table"
+    run_sql "update markers set feed_uri_for_last_read_entry = concat('http://${OPENELIS_HOST}:${OPENELIS_PORT}/openelis/ws/feed/patient',substring(feed_uri_for_last_read_entry, (LENGTH(feed_uri_for_last_read_entry) - LOCATE('/', REVERSE(feed_uri_for_last_read_entry)))+1) ) where feed_uri like '%openelis/ws/feed/patient/recent';"
+    run_sql "update markers set feed_uri = 'http://${OPENELIS_HOST}:${OPENELIS_PORT}/openelis/ws/feed/patient/recent' where feed_uri like '%openelis/ws/feed/patient/recent';"
+    run_sql "update failed_events set feed_uri = 'http://${OPENELIS_HOST}:${OPENELIS_PORT}/openelis/ws/feed/patient/recent' where feed_uri like '%openelis/ws/feed/patient/recent';"
 fi
